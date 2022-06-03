53b4e402eacc492c389949f25e0309685e5b8c2a
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-06-03 18:05:52 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jan Andrzejewski on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
