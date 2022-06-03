037ac6112013dacfb5dbc9f8991d696ab0bf15ee
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-06-03 18:11:52 +0200

    Added a stepper that lets users input their sleep time

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 3...12, step: 0.25)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
